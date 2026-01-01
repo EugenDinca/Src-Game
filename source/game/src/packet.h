eae2d9738238f6387e53f2728c3a85af989e5c78
EugenDinca <genugenu22@yahoo.com>
2026-01-01 15:11:53 +0100

    d

--- source/game/src/packet.h
+++ source/game/src/packet.h
@@ -1189,14 +1189,14 @@ typedef struct packet_guild_ranking_send
 {
 	packet_guild_ranking_send()
 	{
-		std::strncpy(szOwnerName, "", sizeof(szOwnerName));
-		std::strncpy(szGuildName, "", sizeof(szGuildName));
+		strncpy(szOwnerName, "", sizeof(szOwnerName));
+		strncpy(szGuildName, "", sizeof(szGuildName));
 	}
 	packet_guild_ranking_send(int32_t c_id, const char* c_guild, const char* c_name, int32_t c_level, int32_t c_point, int32_t c_win, int32_t c_draw, int32_t c_loss)
 		: subheader(GUILD_RANK::SUBHEADER_GUILD_RANKING_SEND), id(c_id), level(c_level), point(c_point), win(c_win), draw(c_draw), loss(c_loss)
 	{
-		std::strncpy(szOwnerName, c_name, sizeof(szOwnerName));
-		std::strncpy(szGuildName, c_guild, sizeof(szGuildName));
+		strncpy(szOwnerName, c_name, sizeof(szOwnerName));
+		strncpy(szGuildName, c_guild, sizeof(szGuildName));
 	}
 	uint8_t	header = HEADER_GC_GUILD_RANKING;
 	uint8_t	subheader = SUBHEADER_GUILD_RANKING_OPEN;
